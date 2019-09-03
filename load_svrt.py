027ee060bf006e30ec26e08e50d60a376c2bddd5
Yihe Lu <Anish@plpxrdj.psychology.nottingham.ac.uk>
2019-09-03 15:26:02 +0100

    Loading parsing. Trial classification.

--- load_svrt.py
+++ load_svrt.py
@@ -1,11 +1,37 @@
 
 import os
-
 import numpy as np
-import cv2
-import pickle
+import csv
+#import cv2
 
-def load_svrt(problem_no):
+
+def load_svrt_parsing(problem_no):
+    X = []
+    y = []
+
+    dirs = './data/parsed_classic'
+    prbm = 'problem_{:0>2d}'.format(problem_no)
+    print('loading ',prbm)
+    for img_index in range(100):
+        if img_index < 50:
+            ans = 1
+        else:
+            ans = 0
+        filename = os.path.join(dirs,prbm,'class_{}'.format(ans),'img_{:0>7d}.txt'.format(img_index))
+
+        with open(filename,'r') as f:
+            parsing = csv.reader(f)
+            prs = []
+            for row in parsing:
+                for i in row:
+                    prs.append(float(i.strip('Shape()borders()contains()')))
+
+        X.append(prs)
+        y.append(ans)
+
+    return (X,y)
+
+def load_svrt_image(problem_no): #this is currently a copy from SVRT-by-RN; the entire load_svrt should be an independent API
     qst = np.zeros(11)
     sample = []
 
@@ -30,31 +56,3 @@ def load_svrt(problem_no):
     rel_test = sample[9000:]
 
     return (rel_train, rel_test)
-
-def load_data():
-    print('loading data...')
-    dirs = './data'
-    filename = os.path.join(dirs,'sort-of-clevr.pickle')
-    with open(filename, 'rb') as f:
-      train_datasets, test_datasets = pickle.load(f)
-    rel_train = []
-    rel_test = []
-    norel_train = []
-    norel_test = []
-    print('processing data...')
-
-    for img, relations, norelations in train_datasets:
-        img = np.swapaxes(img,0,2)
-        for qst,ans in zip(relations[0], relations[1]):
-            rel_train.append((img,qst,ans))
-        for qst,ans in zip(norelations[0], norelations[1]):
-            norel_train.append((img,qst,ans))
-
-    for img, relations, norelations in test_datasets:
-        img = np.swapaxes(img,0,2)
-        for qst,ans in zip(relations[0], relations[1]):
-            rel_test.append((img,qst,ans))
-        for qst,ans in zip(norelations[0], norelations[1]):
-            norel_test.append((img,qst,ans))
-
-    return (rel_train, rel_test, norel_train, norel_test)
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from sklearn.model_selection import cross_val_score
 from sklearn.datasets import load_iris
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn import metrics
 
-X, y = load_iris(return_X_y=True)
+from load_svrt import load_svrt_parsing
+
+X, y = load_svrt_parsing(1)
+X_train, y_train = X, y
+X_test, y_test = X, y
 clf = AdaBoostClassifier(n_estimators=100)
-scores = cross_val_score(clf, X, y, cv=5)
-print(scores.mean())
+model = clf.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
